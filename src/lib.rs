6e3c36743e3317178f1c33f153a1db94bd9c8f01
Deok Hwan Kim <tiger1710p@gmail.com>
2023-02-09 13:02:56 +0900

    Add test

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,23 @@ mod tests {
     use crate::rofl::Rofl;
 
     #[test]
-    fn parse_rofl() -> Result<(), Box<dyn Error>> {
-        let mut rofl = Rofl::new();
-        let parse_result = rofl.parse_rofl("KR-5736537983.rofl");
+    fn only_new() {
+        let rofl = Rofl::new();
 
-        if let Ok(_) = parse_result {
-            let rofl_json = rofl.get_rofl_json();
-            let stats_json = rofl.get_stats_json();
-        }
+        assert!(rofl.get_rofl_json().is_none());
+        assert!(rofl.get_stats_json().is_none());
+    }
+
+    #[test]
+    fn parse_rofl_file() -> Result<(), Box<dyn Error>> {
+        let mut rofl = Rofl::new();
+        let _ = rofl.parse_rofl_file("KR-5736537983.rofl")?;
+
+        let rofl_json = rofl.get_rofl_json();
+        let stats_json = rofl.get_stats_json();
+
+        assert!(rofl_json.is_some());
+        assert!(stats_json.is_some());
 
         Ok(())
     }
