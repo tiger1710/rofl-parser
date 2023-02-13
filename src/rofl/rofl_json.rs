7e975cf8826a9de4b5102cceb7a57ddbd9cc0a24
Deok Hwan Kim <tiger1710p@gmail.com>
2023-02-13 13:16:30 +0900

    Merge statsjson to rofljson

--- src/rofl/rofl_json.rs
+++ src/rofl/rofl_json.rs
@@ -1,8 +1,6 @@
-use std::error::Error;
-
 use serde::{Deserialize, Serialize};
 
-use super::{player::Player, stats_json::StatsJson};
+use super::player::Player;
 
 #[allow(non_snake_case)]
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -11,12 +9,21 @@ pub struct RoflJson {
     pub gameVersion: String,
     pub lastGameChunkId: i32,
     pub lastKeyFrameId: i32,
+    pub statsJson: Vec<Player>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct MockRoflJson {
+    pub gameLength: i32,
+    pub gameVersion: String,
+    pub lastGameChunkId: i32,
+    pub lastKeyFrameId: i32,
     statsJson: String,
 }
 
-impl RoflJson {
-    pub fn parse_stats_json(&self) -> Result<StatsJson, Box<dyn Error>> {
-        let players: Vec<Player> = serde_json::from_str(&self.statsJson)?;
-        Ok(StatsJson::new(players))
+impl MockRoflJson {
+    pub fn parse_stats_json(&self) -> anyhow::Result<Vec<Player>> {
+        Ok(serde_json::from_str(&self.statsJson)?)
     }
 }
